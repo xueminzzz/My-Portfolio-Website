72bda50fc9f06640f6023f4638d50539a568a570
xueminzzz <106852739+xueminzzz@users.noreply.github.com>
2023-10-17 17:41:06 +0800

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,6 +1,3 @@
-export default function cloudinaryLoader({ src, width, quality }) {
-  const params = ['f_auto', 'c_limit', `w_${width}`, `q_${quality || 'auto'}`]
-  return `https://res.cloudinary.com/demo/image/upload/${params.join(
-    ','
-  )}${src}`
-}
+const prefix = process.env.NEXT_PUBLIC_BASE_PATH || ""
+
+export { prefix }
