dd7d76fb1c1105fd3b937be325c6322b65870d01
Ng <xue.min.ng@accenture.com>
2023-11-06 15:42:34 +0800

    pls work

--- pages/api/sendEmail.js
+++ pages/api/sendEmail.js
@@ -1,15 +1,12 @@
-import nodemailer from 'nodemailer'
+import nodemailer from 'nodemailer';
 
 export default async function handler(req, res) {
     if (req.method == "POST") {
-
-        const { name, email, message } = req.body
+        const { name, email, message } = req.body;
 
         if (!name || !email || !message) {
-            return res.status(400).json({ message: "Bad request 1" })
-        }
-
-        else {
+            return res.status(400).json({ message: "Bad request 1" });
+        } else {
             const transporter = nodemailer.createTransport({
                 service: 'gmail',
                 auth: {
@@ -17,30 +14,38 @@ export default async function handler(req, res) {
                     pass: "cerobkpyqbmprisn"
                 },
             });
-            try {
+
+            const sendMailPromise = new Promise((resolve, reject) => {
                 transporter.sendMail({
                     from: "t2988061@gmail.com",
                     to: "t2988061@gmail.com",
                     subject: "New Connection!",
                     text: `Hello,
-        
-                You have a new form entry from:
-                 
-                Name: ${name} 
-                
-                Email: ${email}.
-        
-                Description: ${message}
-        
-                `,
-                })
 
-                return res.status(200).json({ success: true })
+                    You have a new form entry from:
 
+                    Name: ${name}
+
+                    Email: ${email}.
+
+                    Description: ${message}
+                    `,
+                }, (error, info) => {
+                    if (error) {
+                        reject(error);
+                    } else {
+                        resolve(info);
+                    }
+                });
+            });
+
+            try {
+                await sendMailPromise;
+                return res.status(200).json({ success: true });
             } catch (error) {
-                return res.status(400).json({ message: error.message })
+                return res.status(400).json({ message: error.message });
             }
         }
     }
-    return res.status(400).json({ message: "Bad request 2" })
+    return res.status(400).json({ message: "Bad request 2" });
 }
