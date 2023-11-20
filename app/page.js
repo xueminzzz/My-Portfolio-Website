a660291eaa83fa5fd70a6380b77275abb6f0b359
Ng <xue.min.ng@accenture.com>
2023-11-20 16:24:53 +0800

    left with projects

--- app/page.js
+++ app/page.js
@@ -9,7 +9,7 @@ import Link from "next/link";
 export default function HomePage() {
   return (
     <div>
-      <Background />
+      {/* <Background /> */}
       <Navigation />
       <Header />
       <div className={styles["projects-header"]} id="projects">Projects</div>
