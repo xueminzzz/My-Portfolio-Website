8e9a4283b1bbaf069135453911f9da8c8800a33e
Ng <xue.min.ng@accenture.com>
2023-11-20 17:20:38 +0800

    left with projects

--- app/components/background/background-animation.js
+++ app/components/background/background-animation.js
@@ -8,7 +8,7 @@ export default function Background() {
     // Some random colors
     const colors = ["#3CC157", "#2AA7FF", "#1B1B1B", "#FCBC0F", "#F85F36"];
 
-    const numBalls = 20;
+    const numBalls = 30;
     const balls = [];
 
     for (let i = 0; i < numBalls; i++) {
