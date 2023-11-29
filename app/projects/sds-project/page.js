a9ce9a6b06c3778f83664b1b28bb8a5a9c257dbb
Ng <xue.min.ng@accenture.com>
2023-11-29 14:49:54 +0800

    sds

--- app/projects/sds-project/page.js
+++ app/projects/sds-project/page.js
@@ -6,13 +6,25 @@ export default function SDS() {
     return (
         <div className={styles["main-container"]}>
             <Image
-                src={""} priority={true}
+                src={"/sds-project-header.svg"} priority={true}
                 width={0}
                 height={0}
                 className={styles["project-header"]}
             />
-            <div className={styles["title"]}>Redesiging CPF Mobile Application</div>
-            <div className={styles["collaborators"]}>SUTD x CPF x Goggle</div>
+            <div className={styles["title-and-description"]}>
+                <div className={styles["title"]}>Redesiging CPF Mobile Application</div>
+                <div className={styles["collaborators"]}>SUTD x CPF x Goggle</div>
+                <div className={styles["description-container"]}>
+                    <div className={styles["sub-description-container"]}>
+                        <div className={styles["sub-description"]}>Timeline:</div>
+                        <div className={styles["sub-description-details"]}>May to August 2023</div>
+                    </div>
+                    <div className={styles["sub-description-container"]}>
+                        <div className={styles["sub-description"]}>Tools used:</div>
+                        <div className={styles["sub-description-details"]}>Ruby on Rails, Next JS, Selenium</div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
