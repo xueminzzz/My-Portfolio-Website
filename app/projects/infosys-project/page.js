816390eae7c30c3bcdf1ec710a20c5607aefc5fd
Ng <xue.min.ng@accenture.com>
2023-11-29 15:54:23 +0800

    infosys

--- app/projects/infosys-project/page.js
+++ app/projects/infosys-project/page.js
@@ -1,10 +1,29 @@
 import styles from "./page.module.css";
-import Navigation from "@/app/components/navbar";
+import Image from "next/image";
 
 export default function IS() {
     return (
-        <div>
-            {/* <Navigation /> */}
+        <div className={styles["main-container"]}>
+            <Image
+                src={"/infosys-project-header.svg"} priority={true}
+                width={0}
+                height={0}
+                className={styles["project-header"]}
+            />
+            <div className={styles["title-and-description"]}>
+                <div className={styles["title"]}>Grab & Go</div>
+                <div className={styles["collaborators"]}>SUTD</div>
+                <div className={styles["description-container"]}>
+                    <div className={styles["sub-description-container"]}>
+                        <div className={styles["sub-description"]}>Timeline:</div>
+                        <div className={styles["sub-description-details"]}>January to April 2023</div>
+                    </div>
+                    <div className={styles["sub-description-container"]}>
+                        <div className={styles["sub-description"]}>Tools used:</div>
+                        <div className={styles["sub-description-details"]}>Andriod Studios</div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
