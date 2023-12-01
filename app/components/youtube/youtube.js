d385844f835d6d5c51e1c25229624bca1cb076c8
Ng <xue.min.ng@accenture.com>
2023-12-01 13:07:10 +0800

    video

--- app/components/youtube/youtube.js
+++ app/components/youtube/youtube.js
@@ -1,24 +1,28 @@
 import { useRef } from "react";
-import { Video, CloudinaryContext } from "cloudinary-react";
+import { Video, CloudinaryContext} from "cloudinary-react";
 import styles from "./youtube.module.css";
 
 const VideoPlayer = () => {
     const videoRef = useRef();
-
     // Cloudinary configuration
     const cloudinaryCloudName = "ddc6slavq"; // Replace with your Cloudinary cloud name
     const publicId = "SDS_Team_9_CREAITIVESTUDIOS_easyCPF_Final_Product_Video_porvu8"; // Replace with the Cloudinary public ID of your video
+    const posterOptions = {
+        transformation: {
+            startOffset: 116
+        }
+    };
 
     return (
         <CloudinaryContext cloudName={cloudinaryCloudName}>
             <div>
                 <Video
-                    startOffset={116}
                     publicId={publicId}
                     width="100%"
                     controls
                     innerRef={videoRef}
                     className={styles["video"]}
+                    posterOptions={posterOptions}          
                 >
                 </Video>
             </div>
