c3017768a0028e6ca010c70294c483da63d4d617
Ng <xue.min.ng@accenture.com>
2023-12-01 15:33:32 +0800

    fucking thumbnail

--- app/components/youtube/youtube.js
+++ app/components/youtube/youtube.js
@@ -1,5 +1,5 @@
 import { useRef } from "react";
-import { Video, CloudinaryContext} from "cloudinary-react";
+import { Video, CloudinaryContext, Transformation } from "cloudinary-react";
 import styles from "./youtube.module.css";
 
 const VideoPlayer = () => {
@@ -7,11 +7,6 @@ const VideoPlayer = () => {
     // Cloudinary configuration
     const cloudinaryCloudName = "ddc6slavq"; // Replace with your Cloudinary cloud name
     const publicId = "SDS_Team_9_CREAITIVESTUDIOS_easyCPF_Final_Product_Video_porvu8"; // Replace with the Cloudinary public ID of your video
-    const posterOptions = {
-        transformation: {
-            startOffset: 116
-        }
-    };
 
     return (
         <CloudinaryContext cloudName={cloudinaryCloudName}>
@@ -22,7 +17,6 @@ const VideoPlayer = () => {
                     controls
                     innerRef={videoRef}
                     className={styles["video"]}
-                    posterOptions={posterOptions}          
                 >
                 </Video>
             </div>
