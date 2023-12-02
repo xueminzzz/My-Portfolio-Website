52df347fa89957294573de6a2197389766a31e4e
xueminzzz <xuemin2809@gmail.com>
2023-12-02 22:05:40 +0800

    adjusted about me

--- app/about-me/page.js
+++ app/about-me/page.js
@@ -47,6 +47,29 @@ export default function AboutMe() {
     return (
         <div className={styles["main-container"]}>
             <div className={styles["parent-container"]}>
+
+
+                <div className={`${styles["child-container"]} ${styles["container-light"]}`}>
+                    <div className={styles["container-light-top"]}>
+                        <p className={styles["title"]}>About me.</p>
+                        <Link href="/">
+                            <button className={styles["close-button-light"]}></button>
+                        </Link>
+                    </div>
+                    <p className={styles["description"]}>Aspiring Software Developer.</p>
+                    <p className={styles["about-me-content"]}>
+                        I'm Xue Min, a 21-year-old student currently doing my undergraduate degree in <strong>Computer Science
+                            and
+                            Design</strong> at the <a className={styles["link"]} href="https://www.sutd.edu.sg/" target="_blank">Singapore University of Technology and Design
+                                (SUTD)</a>. I am
+                        interested in Software as a Service, where I get to create software solutions to solve real-world
+                        problems.
+                        I am
+                        particularly interested in <strong>front-end development</strong>. When not working
+                        on code, I like to dance.
+                    </p>
+                </div>
+
                 <div className={`${styles["child-container"]} ${styles["container-dark"]}`}>
                     <div className={styles["container-dark-top"]}>
                         <p className={styles["title"]}>Let's chat.</p>
@@ -98,27 +121,6 @@ export default function AboutMe() {
                             <div className={styles["success-message"]}>{successMessage}</div>
                         </form>
                     </div>
-                </div>
-
-                <div className={`${styles["child-container"]} ${styles["container-light"]}`}>
-                    <div className={styles["container-light-top"]}>
-                        <p className={styles["title"]}>About me.</p>
-                        <Link href="/">
-                            <button className={styles["close-button-light"]}></button>
-                        </Link>
-                    </div>
-                    <p className={styles["description"]}>Aspiring Software Developer.</p>
-                    <p className={styles["about-me-content"]}>
-                        I'm Xue Min, a 21-year-old student currently doing my undergraduate degree in <strong>Computer Science
-                            and
-                            Design</strong> at the <a className={styles["link"]} href="https://www.sutd.edu.sg/" target="_blank">Singapore University of Technology and Design
-                                (SUTD)</a>. I am
-                        interested in Software as a Service, where I get to create software solutions to solve real-world
-                        problems.
-                        I am
-                        particularly interested in <strong>front-end development</strong>. When not working
-                        on code, I like to dance.
-                    </p>
                 </div>
             </div>
         </div>
