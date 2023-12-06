50c8cad86aea744ca0e9eec1559c857a10c4a277
Ng <xue.min.ng@accenture.com>
2023-12-06 15:22:55 +0800

    hope this works

--- app/page.js
+++ app/page.js
@@ -11,29 +11,29 @@ export default function HomePage() {
   return (
     <div>
       {/* <Background /> */}
-      <Navigation />
+      {/* <Navigation /> */}
       <Header />
-      <div className={styles["projects-and-footer-container"]}>
-        <div className={styles["projects-container"]} >
-          <div className={styles["projects-header"]} id="projects">Projects</div>
-          <div className={styles["projects"]} id="projects">
-            <Link href="/projects/sds-project" className={styles["projects-link"]}>
-              <Projects
-                image="/sds-project-placeholder.svg"
-                title="Service Design Studio x CPF"
-                description="Redesigning CPF Mobile Application"
-                className={`${styles["image-border"]} ${styles["project1-border"]}`}
-              />
-            </Link>
-            <Link href="/projects/infosys-project" className={styles["projects-link"]}>
-              <Projects
-                image="/infosys-project-placeholder.svg"
-                title="Grab & Go"
-                description="Android Studios Mobile Application"
-                className={`${styles["image-border"]} ${styles["project2-border"]}`}
-              />
-            </Link>
-            {/* <Link href="/projects/compstruc-project" className={styles["projects-link"]}>
+      {/* <div className={styles["projects-and-footer-container"]}> */}
+      <div className={styles["projects-container"]} >
+        <div className={styles["projects-header"]} id="projects">Projects</div>
+        <div className={styles["projects"]} id="projects">
+          <Link href="/projects/sds-project" className={styles["projects-link"]}>
+            <Projects
+              image="/sds-project-placeholder.svg"
+              title="Service Design Studio x CPF"
+              description="Redesigning CPF Mobile Application"
+              className={`${styles["image-border"]} ${styles["project1-border"]}`}
+            />
+          </Link>
+          <Link href="/projects/infosys-project" className={styles["projects-link"]}>
+            <Projects
+              image="/infosys-project-placeholder.svg"
+              title="Grab & Go"
+              description="Android Studios Mobile Application"
+              className={`${styles["image-border"]} ${styles["project2-border"]}`}
+            />
+          </Link>
+          {/* <Link href="/projects/compstruc-project" className={styles["projects-link"]}>
           <Projects
             image="/compstruc-project-placeholder.svg"
             title="NHOMP"
@@ -41,10 +41,10 @@ export default function HomePage() {
             className={`${styles["image-border"]} ${styles["project3-border"]}`}
           />
         </Link> */}
-          </div>
         </div>
-        <Footer />
       </div>
+      {/* <Footer /> */}
+      {/* </div> */}
     </div>
   )
 }
