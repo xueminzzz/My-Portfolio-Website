a7b49ccf39185c0c34ddf10c6c15d1bbcdad9035
Ng <xue.min.ng@accenture.com>
2023-12-08 17:04:49 +0800

    added name logo

--- app/components/navbar/navbar-desktop/index.js
+++ app/components/navbar/navbar-desktop/index.js
@@ -6,12 +6,14 @@ import { useState } from "react";
 
 export default function Navbar({ isOpen, toggle }) {
     return (
-        <div className={styles["main-container"]} >
-            <div className={styles["hamburger-container"]}>
+        <div className={styles["main-container"]}>
+            <div className={isOpen ? styles["hamburger-container-open"] : styles["hamburger-container-close"]}>
+                <div className={isOpen ? styles["name-close"] : styles["name"]}>✿Xue Min✿</div>
                 <button onClick={toggle} className={isOpen ? styles["close-button"] : styles["hamburger"]}>
                 </button>
             </div>
             <nav className={styles["navbar"]}>
+                <div className={styles["name"]}>✿Xue Min✿</div>
                 <ul className={styles["navbar-ul"]}>
                     <Link href="/" className={styles["projects-link"]}>
                         <li className={styles["nav-item"]}>Home</li>
